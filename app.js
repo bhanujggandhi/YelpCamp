650242363fc3ed612f89a5f39ff80d1c2c8e2d60
bhanujggandhi <gandhibhanuj@gmail.com>
2020-04-15 18:53:22 +0530

    Auth added

--- app.js
+++ app.js
@@ -79,7 +79,7 @@ app.get('/campgrounds/:id', function(req, res) {
 // COMMENTS ROUTES
 // ====================
 
-app.get('/campgrounds/:id/comments/new', function(req, res) {
+app.get('/campgrounds/:id/comments/new', isLoggedIn, function(req, res) {
 	// find campground by id
 	Campground.findById(req.params.id, function(err, campground) {
 		if (err) {
@@ -90,7 +90,7 @@ app.get('/campgrounds/:id/comments/new', function(req, res) {
 	});
 });
 
-app.post('/campgrounds/:id/comments', function(req, res) {
+app.post('/campgrounds/:id/comments', isLoggedIn, function(req, res) {
 	//lookup campground using ID
 	Campground.findById(req.params.id, function(err, campground) {
 		if (err) {
@@ -150,6 +150,18 @@ app.post(
 	}),
 	function(req, res) {}
 );
+
+app.get('/logout', function(req, res) {
+	req.logout();
+	res.redirect('/campgrounds');
+});
+
+function isLoggedIn(req, res, next) {
+	if (req.isAuthenticated()) {
+		return next();
+	}
+	res.redirect('/login');
+}
 
 app.listen(3000, function() {
 	console.log('The YelpCamp Server Has Started');
