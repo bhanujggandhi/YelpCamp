6486b87416c4621d483a83c9b0acae20bb9b5d74
bhanujggandhi <gandhibhanuj@gmail.com>
2020-04-18 12:59:40 +0530

    Edit functionality

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -52,6 +52,27 @@ router.get('/:id', function(req, res) {
 	});
 });
 
+// EDIT CAMPGROUND ROUTE
+router.get('/:id/edit', function(req, res) {
+	Campground.findById(req.params.id, function(err, foundCampground) {
+		if (err) {
+			res.redirect('/campgrounds');
+		} else {
+			res.render('campgrounds/edit', { campground: foundCampground });
+		}
+	});
+});
+// UPDATE CAMPGROUND ROUTE
+router.put('/:id', function(req, res) {
+	Campground.findByIdAndUpdate(req.params.id, req.body.campground, function(err, updatedCampground) {
+		if (err) {
+			res.redirect('/campgrounds');
+		} else {
+			res.redirect('/campgrounds/' + req.params.id);
+		}
+	});
+});
+
 //MiddleWare
 function isLoggedIn(req, res, next) {
 	if (req.isAuthenticated()) {
