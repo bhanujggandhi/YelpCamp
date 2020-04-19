e2bb4f3b083dff173cbf71235a4ff264a54055f2
bhanujggandhi <gandhibhanuj@gmail.com>
2020-04-19 20:00:41 +0530

    Commit

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -2,14 +2,6 @@ var express = require('express');
 var router = express.Router();
 var Campground = require('../models/campground');
 var middleware = require('../middleware');
-var NodeGeocoder = require('node-geocoder');
-
-var options = {
-	provider: 'google',
-	httpAdapter: 'https',
-	apiKey: process.env.GEOCODER_API_KEY,
-	formatter: null
-};
 
 var geocoder = NodeGeocoder(options);
 
