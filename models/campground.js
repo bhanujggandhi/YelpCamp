1a84f8b8b5b613a50860729f1dc7811c36d6c1fa
bhanujggandhi <gandhibhanuj@gmail.com>
2020-05-13 08:05:38 +0530

    Formatted code

--- models/campground.js
+++ models/campground.js
@@ -1,25 +1,25 @@
-var mongoose = require('mongoose');
+var mongoose = require("mongoose");
 
 //SCHEMA SETUP
 var campgroundSchema = new mongoose.Schema({
-	name: String,
-	price: String,
-	image: String,
-	description: String,
-	createdAt: { type: Date, default: Date.now },
-	author: {
-		id: {
-			type: mongoose.Schema.Types.ObjectId,
-			ref: 'User'
-		},
-		username: String
-	},
-	comments: [
-		{
-			type: mongoose.Schema.Types.ObjectId,
-			ref: 'Comment'
-		}
-	]
+  name: String,
+  price: String,
+  image: String,
+  description: String,
+  createdAt: { type: Date, default: Date.now },
+  author: {
+    id: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+    username: String,
+  },
+  comments: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Comment",
+    },
+  ],
 });
 
-module.exports = mongoose.model('Campground', campgroundSchema);
+module.exports = mongoose.model("Campground", campgroundSchema);
--- models/user.js
+++ models/user.js
@@ -1,12 +1,12 @@
-var mongoose = require('mongoose');
-var passportLocalMongoose = require('passport-local-mongoose');
+var mongoose = require("mongoose");
+var passportLocalMongoose = require("passport-local-mongoose");
 
 var userSchema = new mongoose.Schema({
-	username: String,
-	password: String,
-	isAdmin: { type: Boolean, default: false }
+  username: String,
+  password: String,
+  isAdmin: { type: Boolean, default: false },
 });
 
 userSchema.plugin(passportLocalMongoose);
 
-module.exports = mongoose.model('User', userSchema);
+module.exports = mongoose.model("User", userSchema);
